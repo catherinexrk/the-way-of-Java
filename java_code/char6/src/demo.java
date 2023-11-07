145787892af9b0c2668a48df0201e113e70719a4
catherinexrk <helloworldxrk@gmail.com>
2023-11-07 16:21:13 +0800

    day3+char6

---
+++ java_code/char6/src/demo.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class demo {
+    public static void main(String[] args) {
+        String name;
+        Scanner in = new Scanner(System.in);
+        
+    }
+}
