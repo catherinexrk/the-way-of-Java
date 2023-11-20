1d1156c0e8d802078a5d0acf7565e93dc041db9f
catherinexrk <helloworldxrk@gmail.com>
2023-11-20 16:48:37 +0800

    test

--- java_code/src/Main.java
+++ java_code/src/Main.java
@@ -1,56 +1,162 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
-public class ConfirmationPage extends JFrame {
-    private JRadioButton option1;
-    private JRadioButton option2;
-    private JButton confirmButton;
+public class Main extends JFrame {
+    private final int WIDTH = 800;
+    private final int HEIGHT = 600;
 
-    public ConfirmationPage() {
-        setTitle("选择确认页面");
+    private PlayerPlane playerPlane;
+    private List<EnemyPlane> enemyPlanes;
+
+    public Main() {
+        setTitle("飞机大战");
+        setSize(WIDTH, HEIGHT);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(300, 200);
-        setLayout(new GridLayout(3, 1));
+        setLocationRelativeTo(null);
 
-        option1 = new JRadioButton("选项1");
-        option2 = new JRadioButton("选项2");
-        confirmButton = new JButton("确认");
+        playerPlane = new PlayerPlane(WIDTH / 2, HEIGHT - 50);
+        enemyPlanes = new ArrayList<>();
 
-        // 创建一个按钮组，确保只有一个单选按钮可以被选中
-        ButtonGroup buttonGroup = new ButtonGroup();
-        buttonGroup.add(option1);
-        buttonGroup.add(option2);
-
-        // 添加确认按钮的点击事件监听器
-        confirmButton.addActionListener(new ActionListener() {
+        addKeyListener(new KeyListener() {
             @Override
-            public void actionPerformed(ActionEvent e) {
-                if (option1.isSelected()) {
-                    JOptionPane.showMessageDialog(null, "您选择了选项1");
-                } else if (option2.isSelected()) {
-                    JOptionPane.showMessageDialog(null, "您选择了选项2");
-                } else {
-                    JOptionPane.showMessageDialog(null, "请选择一个选项");
-                }
+            public void keyTyped(KeyEvent e) {}
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                playerPlane.handleKeyPress(e.getKeyCode());
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+                playerPlane.handleKeyRelease(e.getKeyCode());
             }
         });
 
-        // 将组件添加到窗口中
-        add(option1);
-        add(option2);
-        add(confirmButton);
+        setFocusable(true);
 
-        setVisible(true);
+        Timer timer = new Timer(20, e -> {
+            update();
+            repaint();
+        });
+        timer.start();
+    }
+
+    private void update() {
+        playerPlane.move();
+
+        // 移动敌机
+        for (EnemyPlane enemyPlane : enemyPlanes) {
+            enemyPlane.move();
+        }
+
+        // 随机生成敌机
+        if (Math.random() < 0.02) {
+            enemyPlanes.add(new EnemyPlane(new Random().nextInt(WIDTH), 0));
+        }
+
+        // 检查碰撞
+        checkCollisions();
+    }
+
+    private void checkCollisions() {
+        Rectangle playerBounds = playerPlane.getBounds();
+
+        // 检查与敌机的碰撞
+        for (EnemyPlane enemyPlane : new ArrayList<>(enemyPlanes)) {
+            Rectangle enemyBounds = enemyPlane.getBounds();
+            if (playerBounds.intersects(enemyBounds)) {
+                JOptionPane.showMessageDialog(this, "游戏结束！", "游戏结束", JOptionPane.INFORMATION_MESSAGE);
+                System.exit(0);
+            }
+        }
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+        playerPlane.draw(g);
+
+        // 绘制敌机
+        for (EnemyPlane enemyPlane : enemyPlanes) {
+            enemyPlane.draw(g);
+        }
     }
 
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                new ConfirmationPage();
-            }
+        SwingUtilities.invokeLater(() -> {
+           Main planeGame = new Main();
+            planeGame.setVisible(true);
         });
+    }
+}
+
+class PlayerPlane {
+    private int x, y;
+    private int speed = 5;
+
+    public PlayerPlane(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void move() {
+        x += speed;
+        if (x < 0) x = 0;
+        if (x > 750) x = 750;
+    }
+
+    public Rectangle getBounds() {
+        return new Rectangle(x, y, 50, 30);
+    }
+
+    public void handleKeyPress(int keyCode) {
+        if (keyCode == KeyEvent.VK_LEFT) {
+            speed = -5;
+        } else if (keyCode == KeyEvent.VK_RIGHT) {
+            speed = 5;
+        }
+    }
+
+    public void handleKeyRelease(int keyCode) {
+        if (keyCode == KeyEvent.VK_LEFT || keyCode == KeyEvent.VK_RIGHT) {
+            speed = 0;
+        }
+    }
+
+    public void draw(Graphics g) {
+        g.setColor(Color.BLUE);
+        g.fillRect(x, y, 50, 30);
+    }
+}
+
+class EnemyPlane {
+    private int x, y;
+    private int speed = 3;
+
+    public EnemyPlane(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void move() {
+        y += speed;
+        if (y > 600) {
+            y = 0;
+            x = new Random().nextInt(750);
+        }
+    }
+
+    public Rectangle getBounds() {
+        return new Rectangle(x, y, 50, 30);
+    }
+
+    public void draw(Graphics g) {
+        g.setColor(Color.RED);
+        g.fillRect(x, y, 50, 30);
     }
 }
