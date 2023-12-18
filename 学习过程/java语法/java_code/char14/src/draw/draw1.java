78983ea5dd9fc75a652828f492ec79a7203ed838
catherinexrk <helloworldxrk@gmail.com>
2023-12-18 19:18:28 +0800

    day43 char14（未完结）

---
+++ 学习过程/java语法/java_code/char14/src/draw/draw1.java
@@ -0,0 +1,24 @@
+package draw;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * @author 陶笛
+ */
+public class draw1 {
+    public static void main(String[] args) {
+
+    }
+}
+
+//定义mypanel类，继承JPanel类
+//graphics g 是一个画笔
+
+class Mypanel extends JPanel{
+
+    @Override
+    public void paint(Graphics g) { //绘图方法
+        super.paint(g);
+    }
+}
