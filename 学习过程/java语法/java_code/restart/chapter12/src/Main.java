3285cef484795c75ce3924be0e421a0f9b977961
catherinexrk <helloworldxrk@gmail.com>
2024-01-23 19:19:39 +0800

    test code

--- 学习过程/java语法/java_code/restart/chapter12/src/Main.java
+++ 学习过程/java语法/java_code/restart/chapter12/src/Main.java
@@ -1,5 +1,28 @@
+import java.security.PrivateKey;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
     }
 }
+
+enum Season{
+    SPRING("spring","warm"),SUMMER("summer","hot");
+
+    private String name;
+    private String descc;
+
+    private Season(String name, String descc) {
+        this.name = name;
+        this.descc = descc;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescc() {
+        return descc;
+    }
+}
+
